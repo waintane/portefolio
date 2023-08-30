4a5c9cc9752c4a846167b79ae57419b56fd7d9ba
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2023-08-30 09:54:42 -0400

    ajout animation

--- src/components/CarteCompetence.tsx
+++ src/components/CarteCompetence.tsx
@@ -1,3 +1,6 @@
+import {useRef} from "react";
+import {useInView} from "framer-motion";
+
 interface IPROPS{
     id: number
     icon: string,
@@ -6,15 +9,24 @@ interface IPROPS{
 }
 
 const CarteCompetence = ({icon, titre, liste, id}: IPROPS) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
     return(
-        <div className="carte">
-            <img src={icon} alt="" />
-            <h3> {titre} </h3>
-            <div className="liste">
-                {liste.map(e =>
-                <p key = {id++}> {e} </p>)}
+        <section ref={ref}>
+            <div className="carte" style={{
+                transform: isInView ? "none" : "translateY(100px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+            }}
+            >
+                <img src={icon} alt="" />
+                <h3> {titre} </h3>
+                <div className="liste">
+                    {liste.map(e =>
+                    <p key = {id++}> {e} </p>)}
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
--- src/components/CarteProjet.tsx
+++ src/components/CarteProjet.tsx
@@ -1,3 +1,6 @@
+import {useRef} from "react";
+import {useInView} from "framer-motion";
+
 interface IPROPS{
     id:number,
     titre: string,
@@ -9,29 +12,37 @@ interface IPROPS{
 }
 
 const CarteProjet = ({id, titre, img, description, github, projet, outil}: IPROPS) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
     return(
-        <div className="carte">
-            <div className="container1">
-                <h3>{titre}</h3>
-                <img src={img} alt="" />
+        <section ref={ref}>
+            <div className="carte" style={{
+                transform: isInView ? "none" : "translateY(100px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.5s 0.5s"
+            }}>
+                <div className="container1">
+                    <h3>{titre}</h3>
+                    <img src={img} alt="" />
+                </div>
+                <div className="container2">
+                    <div className="description">
+                        <p>
+                            {description}
+                        </p>
+                    </div>
+                    <div className="bouton">
+                        <div><a href={github} target="_blank" rel="noopener noreferrer">GitHub</a></div>
+                        <div><a href={projet} target="_blank" rel="noopener noreferrer">Projet</a></div>
+                    </div>
+                    <div className="outil">
+                        {outil.map(e =>
+                            <div key = {id++}> {e} </div>
+                        )}
+                    </div>
+                </div>
             </div>
-            <div className="container2">
-                <div className="description">
-                    <p>
-                        {description}
-                    </p>
-                </div>
-                <div className="bouton">
-                    <div><a href={github} target="_blank" rel="noopener noreferrer">GitHub</a></div>
-                    <div><a href={projet} target="_blank" rel="noopener noreferrer">Projet</a></div>
-                </div>
-                <div className="outil">
-                    {outil.map(e =>
-                        <div key = {id++}> {e} </div>
-                    )}
-                </div>
-            </div>
-        </div>
+        </section>
     )
 }
 
